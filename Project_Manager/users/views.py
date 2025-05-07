1678d6a49104b70e7faee69f8fe4aeb78e315604
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-08 01:28:21 +0500

    Templates for authorization and registration

--- Project_Manager/users/views.py
+++ Project_Manager/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_user(request):
+    return render(request, 'users/login.html')
+
+def register_user(request):
+    return render(request, 'users/register.html')
