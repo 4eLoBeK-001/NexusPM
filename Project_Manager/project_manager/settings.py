cf16625182f20f236b24ca39bb836b732659e049
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-08 00:45:03 +0500

    added model teams

--- Project_Manager/project_manager/settings.py
+++ Project_Manager/project_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'projects',
     'users',
+    'teams',
     'django_extensions',
 ]
 
--- Project_Manager/teams/models.py
+++ Project_Manager/teams/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'Пороект - {self.name}'
