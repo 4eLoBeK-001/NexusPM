e9e241c5a7d325d68e56cffda697a47516434584
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-18 22:05:15 +0500

    new model project

--- Project_Manager/projects/models.py
+++ Project_Manager/projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from teams.models import Team
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='project_avatars/', blank=True, null=True)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, related_name='projects', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
--- Project_Manager/teams/models.py
+++ Project_Manager/teams/models.py
@@ -13,7 +13,8 @@ def get_random_color():
 class Team(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='author_teams', null=True)
+    team_member = models.ManyToManyField(get_user_model(), related_name='member_teams')
     image = models.ImageField(upload_to='team_avatars/', blank=True, null=True)
     color = models.CharField(max_length=10, default=get_random_color)
     created_at = models.DateTimeField(auto_now_add=True)
