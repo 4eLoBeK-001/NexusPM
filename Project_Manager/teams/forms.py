b6f928fa01740a130ca4a9626ec923fec4492d46
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-19 20:39:20 +0500

    optimized

--- Project_Manager/teams/forms.py
+++ Project_Manager/teams/forms.py
@@ -22,7 +22,7 @@ class AddTeamForm(forms.ModelForm):
 
     name = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'input input-bordered w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent',
+            'class': 'input input-bordered text-lg w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent',
             'placeholder': 'Имя проекта'
         }),
         label='Название'
@@ -30,26 +30,26 @@ class AddTeamForm(forms.ModelForm):
     
     description = forms.CharField(
         widget=forms.Textarea(attrs={
-            'class': 'textarea textarea-bordered w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent h-24',
+            'class': 'textarea textarea-bordered text-base w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent h-24',
             'placeholder': 'Описание проекта',
             'rows': 5,
             'cols': 40
         }),
-        label='Description',
+        label='Описание',
         required=False
     )
     
-    team_member = forms.ModelMultipleChoiceField(
-        widget=forms.SelectMultiple(attrs={
-            'class': 'select select-primary select-bordered w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent h-auto min-h-12 py-2',
-            'multiple': 'multiple',
-            'data-placeholder': 'Выбор участников команды'
-        }),
-        queryset=get_user_model().objects.all(),
-        label='Участники команды',
-        help_text='С зажатым Ctrl/Cmd выберите несколько человек'
-    )
+    # team_member = forms.ModelMultipleChoiceField(
+    #     widget=forms.SelectMultiple(attrs={
+    #         'class': 'select select-primary select-bordered w-full bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent h-auto min-h-12 py-2',
+    #         'multiple': 'multiple',
+    #         'data-placeholder': 'Выбор участников команды'
+    #     }),
+    #     queryset=get_user_model().objects.all(),
+    #     label='Участники команды',
+    #     help_text='С зажатым Ctrl/Cmd выберите несколько человек'
+    # )
 
     class Meta:
         model = Team
-        fields = ('name', 'description', 'team_member')
+        fields = ('name', 'description')
