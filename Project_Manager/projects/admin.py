9b1ba1fc70c313f1b22f8e824d9d7726d32ffea2
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-26 21:30:30 +0500

    A project practicant has been added to the project

--- Project_Manager/projects/admin.py
+++ Project_Manager/projects/admin.py
@@ -4,10 +4,18 @@ from .models import Project
 # Register your models here.
 
 
-
+class ProjectMember(admin.TabularInline):
+    model = Project.project_members.through
+    extra = 4
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'team', 'created_at', 'updated_at')
     list_display_links = ('name', 'team')
+
+    readonly_fields = ('project_members',)
+
+    inlines = [
+        ProjectMember
+    ]
