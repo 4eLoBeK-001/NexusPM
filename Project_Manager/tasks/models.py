91c600a472f379843081d490e82484407a68b4ed
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-31 21:00:29 +0500

    task list part 4 view tasks

--- Project_Manager/tasks/models.py
+++ Project_Manager/tasks/models.py
@@ -1,13 +1,21 @@
+import random
+
 from django.db import models
 from django.contrib.auth import get_user_model
 
 from projects.models import Project
 
-# Create your models here.
+def get_colors():
+    return ['sky', 'blue', 'red', 'orange', 'lime', 'teal', 'violet', 'zinc']
+
+def get_random_color():
+    return random.choice(get_colors())
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    color = models.CharField(max_length=10, default=get_random_color)
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='creator', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='tasks', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- Project_Manager/tasks/views.py
+++ Project_Manager/tasks/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from projects.models import Project
+from tasks.models import Task
 
 # Create your views here.
 
 def task_list(request, project_pk, team_pk):
-    return render(request, 'tasks/task_list.html')
+    project = get_object_or_404(Project, pk=project_pk)
+    tasks = Task.objects.filter(project=project)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/task_list.html', context)
