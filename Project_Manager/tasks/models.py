bf3932eb5921a9bcac3e96acf6004c0c92dd7648
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-06-02 23:29:59 +0500

    refac

--- Project_Manager/tasks/models.py
+++ Project_Manager/tasks/models.py
@@ -18,7 +18,7 @@ class Task(models.Model):
     color = models.CharField(max_length=10, default=get_random_color)
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='creator', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='tasks', null=True)
-    tag = models.ManyToManyField('Tag', related_name='tags')
+    tag = models.ManyToManyField('Tag', related_name='tags', null=True, blank=True)
     status = models.ForeignKey('Status', on_delete=models.CASCADE, related_name='statuses', default='Новая', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -30,6 +30,7 @@ class Task(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=25)
     color = models.ForeignKey('Color', on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -45,6 +46,7 @@ class Status(models.Model):
 
 class Color(models.Model):
     name = models.CharField(max_length=50)
+    color_name = models.CharField(max_length=50)
     bg_color = models.CharField(max_length=50)
     text_color = models.CharField(max_length=50)
     border_color = models.CharField(max_length=50)
