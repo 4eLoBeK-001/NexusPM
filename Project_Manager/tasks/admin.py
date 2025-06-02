a8d11897bd79c10fd65b10ef2d161c3b7dcf9b0e
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-06-02 21:06:50 +0500

    added tags

--- Project_Manager/tasks/admin.py
+++ Project_Manager/tasks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from tasks.models import Task
+from tasks.models import Task, Tag, TagColor
 
 # Register your models here.
 
 admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(TagColor)
--- Project_Manager/tasks/models.py
+++ Project_Manager/tasks/models.py
@@ -18,5 +18,25 @@ class Task(models.Model):
     color = models.CharField(max_length=10, default=get_random_color)
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='creator', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='tasks', null=True)
+    tag = models.ManyToManyField('Tag', related_name='tags')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+    color = models.ForeignKey('TagColor', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+class TagColor(models.Model):
+    name = models.CharField(max_length=50)
+    bg_color = models.CharField(max_length=50)
+    text_color = models.CharField(max_length=50)
+    border_color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
