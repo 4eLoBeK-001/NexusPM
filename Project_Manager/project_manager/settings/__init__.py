edf0060b438dd01da4d0a1eb52b20e0f7ceb17ff
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-06-03 23:02:02 +0500

    Optimizing the settings file

---
+++ Project_Manager/project_manager/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+from .database import *
+from .app_vars import *
+from .apps import *
---
+++ Project_Manager/project_manager/settings/base.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = 'django-insecure-@%qwjoe$%0#h))%vje#f(4qgqwng*d=qx7aioyn@9mv_nq_-92'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+ROOT_URLCONF = 'project_manager.urls'
+
