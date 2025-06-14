5e42acc043b427ccddbaecee521c910ba8a7fe41
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-06-14 19:14:50 +0500

    marking a task as completed

--- Project_Manager/tasks/models.py
+++ Project_Manager/tasks/models.py
@@ -28,7 +28,7 @@ class Task(models.Model):
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='creator', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='tasks', null=True)
     tag = models.ManyToManyField('Tag', related_name='tags', null=True, blank=True)
-    status = models.ForeignKey('Status', on_delete=models.CASCADE, related_name='statuses', default='Новая', null=True)
+    status = models.ForeignKey('Status', on_delete=models.SET_NULL, related_name='statuses', default='Новая', null=True)
     priority = models.CharField(max_length=20, choices=PriprityChoices.choices, default=PriprityChoices.NOT_SPECIFIED)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -40,7 +40,7 @@ class Task(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=25)
-    color = models.ForeignKey('Color', on_delete=models.CASCADE, null=True)
+    color = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tags', null=True)
 
     def __str__(self):
@@ -49,7 +49,7 @@ class Tag(models.Model):
 
 class Status(models.Model):
     name = models.CharField(max_length=25, blank=False)
-    color = models.ForeignKey('Color', on_delete=models.CASCADE, null=True, blank=False)
+    color = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True, blank=False)
     is_completed = models.BooleanField(default=False, blank=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='statuses', null=True)
 
