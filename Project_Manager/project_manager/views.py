f16247dfb0b29308ccaba9b3cc7cc2ac526952b5
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-07-14 20:47:50 +0500

    notifications

--- Project_Manager/project_manager/views.py
+++ Project_Manager/project_manager/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'home-page.html')
+
+
+def notifications(request):
+    return render(request, 'notifications.html')
