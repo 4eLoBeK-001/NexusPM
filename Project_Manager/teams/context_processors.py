a25e978191167f707d37349b5418f22cdeb05e60
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-08-02 23:07:14 +0500

    checking the role processor

--- Project_Manager/teams/context_processors.py
+++ Project_Manager/teams/context_processors.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 
 from .models import Team
+from users.models import TeamMember
 from .forms import AddModalTeamForm
-
 
 def all_teams_processor(request):
     if request.user.is_authenticated:
@@ -16,3 +16,19 @@ def all_teams_processor(request):
 
 def project_form_processor(request):
     return {'context_form': AddModalTeamForm()}
+
+
+def role_proccessor(request):
+    team_pk = None
+    if hasattr(request, 'resolver_match'):
+        team_pk = request.resolver_match.kwargs.get('pk')
+
+    if not request.user.is_authenticated or not team_pk:
+        return {}
+
+    member = TeamMember.objects.get(team_id=team_pk, user=request.user)
+    print(member.role)
+    return {
+        'your_role': member.role
+    }
+
