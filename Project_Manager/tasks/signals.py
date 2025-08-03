a9c405b7649aee5b241260f830dc1190fd3cc61f
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-08-03 20:43:36 +0500

    notifications for all actions that have occurred with tasks, tags, and comments

--- Project_Manager/tasks/signals.py
+++ Project_Manager/tasks/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_save, pre_delete, post_delete
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
 from django.dispatch import receiver
 
 from teams.middleware import get_current_user
@@ -9,20 +9,55 @@ from .models import Task, Tag, Status, TaskImage, Comment
 @receiver(signal=post_save, sender=Task)
 def create_task_signal(sender, instance, created, *args, **kwargs):
     if created:
-        print(f'Команда - {instance.project.team.name} \n'
-              f'Проект - {instance.project.name} \n'
-              f'{get_current_user()} создал задачу {instance.name}'
- 
-        )
+        if instance.parent_task:
+            print(f'Команда - {instance.project.team.name} \n'
+                  f'Проект - {instance.project.name} \n'
+                  f'{get_current_user()} создал подзадачу {instance.name} к задаче {instance.parent_task}'
+                  )
+        else:
+            print(f'Команда - {instance.project.team.name} \n'
+                  f'Проект - {instance.project.name} \n'
+                  f'{get_current_user()} создал задачу {instance.name}'
+            )
 
 
 @receiver(signal=post_delete, sender=Task)
-def create_task_signal(sender, instance, *args, **kwargs):
+def delete_task_signal(sender, instance, *args, **kwargs):
     print(f'Команда - {instance.project.team.name} \n'
             f'Проект - {instance.project.name} \n'
             f'{get_current_user()} удалил задачу {instance.name}'
 
     )
+
+
+@receiver(signal=pre_save, sender=Task)
+def change_task_signal(sender, instance, *args, **kwargs):
+    if not instance.pk:
+        return
+
+    old_instance = Task.objects.get(pk=instance.pk)
+
+    changes = {}
+
+    if old_instance.name != instance.name:
+        changes['name'] = (old_instance.name, instance.name)
+    if old_instance.description != instance.description:
+        changes['description'] = (old_instance.description, instance.description)
+    if old_instance.status != instance.status:
+        changes['status'] = (old_instance.status, instance.status)
+    if old_instance.priority != instance.priority:
+        changes['priority'] = (old_instance.priority, instance.priority)
+
+
+    if changes:
+        if changes.get('name'):
+            print(f'{get_current_user()} изменил имя задачи с {old_instance.name} на {instance.name}')
+        if changes.get('description'):
+            print(f'{get_current_user()} изменил описание задачи {instance.name} с {old_instance.description} на {instance.description}')
+        if changes.get('status'):
+            print(f'{get_current_user()} изменил статус задачи {instance.name} с {old_instance.status} на {instance.status}')
+        if changes.get('priority'):
+            print(f'{get_current_user()} изменил приоритет задачи {instance.name} с {old_instance.priority} на {instance.priority}')
 
 
 @receiver(signal=post_save, sender=Tag)
@@ -33,12 +68,24 @@ def create_tag_signal(sender, instance, created, *args, **kwargs):
               f'{get_current_user()} создал новый тег {instance}'
         )
 
+
+@receiver(signal=m2m_changed, sender=Task.tag.through)
+def change_tag_signal(sender, instance, action, *args, **kwargs):
+    tags_list = [tag.name for tag in instance.tag.all()]
+
+    if action in ['post_add', 'post_remove', 'post_clear']:
+        print(f'Пользователь {get_current_user()} в задаче {instance.name} \n'
+              f'Изменил теги: {', '.join(tags_list)}'
+        )
+
+
 @receiver(signal=post_delete, sender=Tag)
 def delete_tag_signal(sender, instance, *args, **kwargs):
     print(f'Команда - {instance.project.team.name} \n' 
             f'Проект - {instance.project.name} \n' 
             f'{get_current_user()} удалил тег {instance}'
     )
+
 
 @receiver(signal=post_save, sender=Status)
 def create_tag_signal(sender, instance, created, *args, **kwargs):
@@ -48,6 +95,7 @@ def create_tag_signal(sender, instance, created, *args, **kwargs):
               f'{get_current_user()} создал новый статус {instance}'
         )
 
+
 @receiver(signal=post_delete, sender=Status)
 def delete_tag_signal(sender, instance, *args, **kwargs):
     print(f'Команда - {instance.project.team.name} \n' 
@@ -56,4 +104,16 @@ def delete_tag_signal(sender, instance, *args, **kwargs):
     )
 
 
+@receiver(signal=post_save, sender=Comment)
+def create_comment_tag(sender, instance, created, *args, **kwargs):
+    if created:
+        print(f'Задача - {instance.task.name} \n'
+              f'{get_current_user()} оставил комментарий: {instance.content}'
+        )
 
+
+@receiver(signal=post_delete, sender=Comment)
+def create_comment_tag(sender, instance, *args, **kwargs):
+        print(f'Задача - {instance.task.name} \n'
+              f'{get_current_user()} удалил комментарий: {instance.content}'
+        )
