3a6c20619470a8c52a9c3b83b00a1c4fc0b2c71b
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-08-03 15:21:26 +0500

    tracking team changes for signals

--- Project_Manager/teams/signals.py
+++ Project_Manager/teams/signals.py
@@ -1,5 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
 from .models import Team
 from users.models import TeamMember, User
 
@@ -12,3 +14,20 @@ def signal_create(sender, instance, created, *args, **kwargs):
             team=instance,
             role='Creator'
         )
+
+@receiver(signal=pre_save, sender=Team)
+def pre_signal(sender, instance, *args, **kwargs):
+    old_instance = get_object_or_404(Team, pk=instance.id)
+
+
+    if old_instance.name != instance.name:
+        print('Изменено имя')
+    if old_instance.description != instance.description:
+        print('Изменено описание')
+    if old_instance.image != instance.image:
+        print('Изменено фото')
+
+
+@receiver(signal=post_save, sender=Team)
+def signal_notif(sender, instance, created, *args, **kwargs):
+    ...
