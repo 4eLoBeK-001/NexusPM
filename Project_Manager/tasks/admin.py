1adce5cb219bcaed2e9d10d6ab1d31f82b8d5ba4
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-08-10 00:51:27 +0500

    task administration part 1

--- Project_Manager/tasks/admin.py
+++ Project_Manager/tasks/admin.py
@@ -4,9 +4,84 @@ from tasks.models import Task, Tag, Status, Color, Comment, TaskImage
 
 # Register your models here.
 
-admin.site.register(Task)
-admin.site.register(Tag)
-admin.site.register(Status)
-admin.site.register(Color)
-admin.site.register(Comment)
+class TagTabularInline(admin.TabularInline):
+    model = Task.tag.through
+    extra = 4
+    max_num = 8
+    classes = ('collapse',)
+
+class ExecutorTabularInline(admin.TabularInline):
+    model = Task.executor.through
+    extra = 4
+    max_num = 8
+    classes = ('collapse',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'project', 'priority', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
+    list_filter = ('project__name', 'created_at')
+    search_fields = ('name', 'project__name', 'creator')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'created_at', 'updated_at'),
+        }),
+        ('Общая информация', {
+            'description': '<h1>Общая информация</h1>',
+            'fields': ('description', 'creator', 'project', 'color', 'parent_task'),
+        }),
+        ('Прочая информация', {
+            'description': '<h1>Теги, статусы и приоритет</h1>',
+            'fields': ('tag', 'status', 'priority'),
+            'classes': ('collapse',)
+        })
+    )
+
+    readonly_fields = ('created_at', 'updated_at')
+    
+    inlines = [
+        TagTabularInline,
+        ExecutorTabularInline
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'project')
+    list_filter = ('name', 'project__name')
+    search_fields = ('name', 'project__name')
+
+    fields = ('name', 'project', 'color')
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'is_completed', 'color')
+    list_filter = ('is_completed', 'project__name')
+    search_fields = ('name', 'project__name')
+    
+    fields = ('name', 'project', 'is_completed', 'color')
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color_name', 'bg_color', 'text_color', 'border_color')
+    list_filter = ('name', 'color_name')
+    search_fields = ('name', 'color_name')
+    
+    readonly_fields = ('name', 'color_name', 'bg_color', 'text_color', 'border_color')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'author', 'task', 'created_at')
+    ordering = ('-created_at',)
+    list_filter = ('content', 'author__username', 'task__name')
+    search_fields = ('content', 'author__username', 'task__name')
+
+    fields = ('content', 'author', 'task', 'created_at')
+    readonly_fields = ('created_at',)
+
 admin.site.register(TaskImage)
