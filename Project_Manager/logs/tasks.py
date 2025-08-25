50226703fe16a13696a507e9e3d80ba84d33e997
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-08-25 16:18:46 +0500

    celery task is to delete logs if they exist for more than 7 days

--- Project_Manager/logs/tasks.py
+++ Project_Manager/logs/tasks.py
@@ -1,10 +1,9 @@
 from celery import shared_task
 
 from django.utils import timezone
-from django.db import transaction
+from django.db import transaction, DatabaseError
 
 from datetime import timedelta
-import logging
 from .models import ActionLog
 
 
@@ -14,12 +13,19 @@ def clean_logs():
     Логи которые существуют больше 7 дней удаляются.
     """
     seven_days_ago = timezone.now() - timedelta(days=7)
-    
-    old_logs = ActionLog.objects.filter(created_at__lt=seven_days_ago)
-    count = old_logs.count()
-    if count > 0:
-        old_logs.delete()
-        return f'Удалено {count} записей'        
-    else:
 
-        return f'Нет записей для удаления'
+    try:
+        with transaction.atomic():
+
+            old_logs = ActionLog.objects.filter(created_at__lt=seven_days_ago)
+            count = old_logs.count()
+            if count > 0:
+                old_logs.delete()
+                return f'Удалено {count} записей'        
+            else:
+                return f'Нет записей для удаления'
+
+    except DatabaseError as e:
+        return f'Ошибка базы данных при удалении логов: {e}'
+    except Exception as e:
+        return f'Неожиданная ошибка: {e}'
